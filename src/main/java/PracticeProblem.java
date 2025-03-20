a994a138f7fb8000ec9bb9e0ef1c27d6f7d01bc3
jamin-xie-ics4u1-02-25 <Jamin.xie@student.tdsb.on.ca>
2025-03-20 14:17:03 +0000

    Update Change

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,58 @@
+import java.text.FieldPosition;
+import java.io.*;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
+	public static String readFile(String filename) {
+		String word = "";
+		BufferedReader inputStream = null;
 
+		try {
+			inputStream = new BufferedReader(new FileReader(filename));
+			String line;
+
+			while ((line = inputStream.readLine()) != null) {
+				if (!word.isEmpty()) {
+					word += "\n";
+				}
+				word += line;
+			}
+		} catch (IOException e) {
+		} finally {
+			try {
+				if (inputStream != null) {
+					inputStream.close();
+				}
+			} catch (IOException e) {
+			}
+		}
+		return word;
+	}
+
+	public static String backwardsReadFile(String filename) {
+		String word = "";
+		BufferedReader inputStream = null;
+
+		try {
+			inputStream = new BufferedReader(new FileReader(filename));
+			int c;
+
+			while ((c = inputStream.read()) != -1) {
+				word = (char)c + word;
+			}
+		} catch (IOException e) {
+		} finally {
+			try {
+				if (inputStream != null) {
+					inputStream.close();
+				}
+			} catch (IOException e) {
+			}
+		}
+		return word.trim();
+	}
 }
